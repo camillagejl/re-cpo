d80debea02062448a57da4ae1282162501e5062c
Camilla Gejl Dilling-Hansen <camilla@dilling.co>
2022-01-13 13:41:48 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container  >
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard" },
+      { title: "Photos", icon: "mdi-image" },
+      { title: "About", icon: "mdi-help-box" },
+    ],
+  }),
+};
+</script>
