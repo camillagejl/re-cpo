bb4be54d79389108d25874d600f9667ba16cd52e
Camilla Gejl Dilling-Hansen <camilla@dilling.co>
2022-01-19 21:24:36 +0100

    Made separate Step component, so it would be easier to display- and hide step images2

---
+++ src/components/recipeStep.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="d-flex align-center">
+      <p class="mx-3 mb-0">
+        <v-icon>
+          mdi-menu
+        </v-icon>
+      </p>
+
+      <v-text-field
+        v-model="step.description"
+        style="width: 10%"
+        outlined
+        dense
+        hide-details
+      >
+      </v-text-field>
+
+      <p class="mx-3 mb-0">
+        <v-img
+          v-if="step.images.length > 0"
+          :src="require('../assets/placeholders/' + step.images[0].image_url)"
+          height="40"
+          width="40"
+          @click="showImages = !showImages"
+        ></v-img>
+
+        <v-img
+          v-else
+          :src="require('../assets/placeholders/placeholder_add.png')"
+          height="40"
+          width="40"
+        ></v-img>
+
+      </p>
+    </v-col>
+
+    <v-expand-transition>
+      <v-row v-show="showImages">
+
+        <v-col
+          class="ml-15 d-flex"
+        >
+          <v-img
+            v-for="image in step.images" :key="image.id"
+            :src="require('../assets/placeholders/' + image.image_url)"
+            height="60"
+            max-width="60"
+            contain
+          ></v-img>
+        </v-col>
+
+      </v-row>
+    </v-expand-transition>
+
+  </v-row>
+</template>
+
+<script>
+
+import { mapState } from "vuex";
+
+export default {
+  name: "RecipeStep",
+
+  data: () => ({
+    showImages: false
+  }),
+
+  props: {
+    step: Object
+  },
+
+  computed: {
+    ...mapState([
+      "categories",
+      "recipeTags",
+      "servingTypes",
+      "nutritionUnits",
+      "recipes",
+      "measuring_units"
+    ])
+  }
+};
+</script>
--- src/views/EditRecipe.vue
+++ src/views/EditRecipe.vue
@@ -365,63 +365,11 @@
 
                 <v-col cols="6">
 
-                  <v-row v-for="(step) in steps" :key="step.id">
-                    <v-col cols="12" class="d-flex align-center">
-                      <p class="mx-3 mb-0">
-                        <v-icon>
-                          mdi-menu
-                        </v-icon>
-                      </p>
-
-                      <v-text-field
-                        v-model="step.description"
-                        style="width: 10%"
-                        outlined
-                        dense
-                        hide-details
-                      >
-                      </v-text-field>
-
-                      <p class="mx-3 mb-0">
-                        <v-img
-                          v-if="step.images.length > 0"
-                          :src="require('../assets/placeholders/' + step.images[0].image_url)"
-                          height="40"
-                          width="40"
-                          @click="showImages = !showImages"
-                        ></v-img>
-
-                        <v-img
-                          v-else
-                          :src="require('../assets/placeholders/placeholder_add.png')"
-                          height="40"
-                          width="40"
-                        ></v-img>
-
-                      </p>
-                    </v-col>
-
-                    <v-expand-transition>
-                      <v-row v-show="showImages">
-
-                        <v-col
-                          class="ml-15 d-flex"
-                        >
-                        <v-img
-                          v-for="image in step.images" :key="image.id"
-                          :src="require('../assets/placeholders/' + image.image_url)"
-                          height="60"
-                          max-width="60"
-                          contain
-                        ></v-img>
-                        </v-col>
-
-                      </v-row>
-                    </v-expand-transition>
-
-                  </v-row>
-                  <v-row>
-                  </v-row>
+                  <RecipeStep
+                    v-for="step in steps"
+                    :key="step.id"
+                    :step="step"
+                  ></RecipeStep>
 
                 </v-col>
 
@@ -455,10 +403,11 @@
 import { mapState } from "vuex";
 import IconHeader from "../components/IconHeader";
 import NutritionInput from "../components/NutritionInput";
+import RecipeStep from "../components/recipeStep";
 
 export default {
   name: "EditRecipe",
-  components: { NutritionInput, IconHeader },
+  components: { RecipeStep, NutritionInput, IconHeader },
   data: () => ({
     panel: [1],
 
